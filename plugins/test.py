64ad939d19291f3626c1a400b6e00cdd5184ebbb
bmeares <bennett.meares@gmail.com>
2024-07-24 14:57:40 -0400

    Remove testing plugin

--- plugins/test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-
-from meerschaum.connectors import Connector, make_connector
-
-@make_connector
-class TestConnector(Connector):
-    pass
